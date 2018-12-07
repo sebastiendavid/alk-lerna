2394e5e6b4e32cefb82bf70b1290ed67cae53c9a
Sébastien David <sdavid@alkemics.com>
2018-12-07 14:32:13 +0100

    alk-lerna-foo: feature 2

--- packages/alk-lerna-foo/index.js
+++ packages/alk-lerna-foo/index.js
@@ -2,5 +2,6 @@ const pkg = require('./package.json');
 
 module.exports = {
   version: () => console.log(`${pkg.name} v${pkg.version}`);
-  feature1: () => 'feature1'
+  feature1: () => 'feature1',
+  feature2: () => 'feature2',
 }
